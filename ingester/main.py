c6b59395f4c23e78a2d94cbb0b6884ad7170b3e8
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2021-10-17 23:20:03 -0500

    Replace influxdb with questdb.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- ingester/main.py
+++ ingester/main.py
@@ -1,9 +1,7 @@
 import json
 import logging
+import socket
 import os
-
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
 
 from kafka import KafkaConsumer
 
@@ -13,20 +11,7 @@ logging.basicConfig()
 logging.getLogger(__name__).setLevel(logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-
-bucket = os.getenv('INFLUX_BUCKET') or 'domain-watcher'
-host = os.getenv('INFLUX_HOST') or 'influx'
-port = os.getenv('INFLUX_PORT') or 8086
-token = os.getenv('INFLUX_TOKEN')
-org = os.getenv('INFLUX_ORG') or 'is-it-still-down?'
-
 brokers = os.getenv('KAFKA_BROKERS') or 'kafka:9092'
-
-
-client = InfluxDBClient(url=f'http://{host}:{port}', token=token, org=org)
-# query_api = client.query_api()
-
-
 
 def kafka_consumer_blocking(brokers=None):
     if brokers is None:
@@ -40,41 +25,77 @@ def kafka_consumer_blocking(brokers=None):
             logger.error('Could not connect to broker %s', e)
     return consumer
 
-def create_point(ping_event: PingEvent):
-    data_point = (
-        Point("domain-ping")
-        .tag("url", ping_event.resource.url)
-        .tag("last_ping_at", int(ping_event.resource.last_ping_at.as_str() if ping_event.resource.last_ping_at else -1))
-        .tag("request_init", int(ping_event.request_init.as_str() if ping_event.request_init else -1))
-        .tag("response_code", ping_event.response_code)
-        .field("response_time", int(ping_event.response_time.as_str() if ping_event.response_time else -1))
-    )
-    return data_point
+def convert_ping_event_to_influxdb_line_protocol(ping_event: PingEvent):
+    # logger.debug('Before tags.')
+    tags = {
+        "url": ping_event.resource.url,
+        "last_ping_at": ping_event.resource.last_ping_at.as_str() if ping_event.resource.last_ping_at else -1,
+        "request_init": ping_event.request_init.as_str() if ping_event.request_init else -1,
+        "response_code": ping_event.response_code
+    }
+
+    # logger.debug('Before fields.')
+    
+    fields = {
+        "response_time": ping_event.response_time.as_str() if ping_event.response_time else -1
+    }
+    measurement = "ping_event"
+    
+    # logger.debug("Before tags, and fields str")
+
+    tags_str = ",".join(map(lambda item: f"{item[0]}={item[1]}", tags.items()))
+    fields_str = ",".join(map(lambda item: f"{item[0]}={item[1]}", fields.items()))
+    
+    result = measurement + "," + tags_str + " " + fields_str
+    logger.debug("Influx Line Protocol: %s", result)
+    return result
+
+
+def handle_multiple_write(data_buffer):
+    try:
+        s = setup_socket()
+        data = "\n".join(map(convert_ping_event_to_influxdb_line_protocol, data_buffer))
+        data += "\n"
+        # logger.debug("DATA: %s", data)
+        s.sendall(data.encode())
+        logger.info("Number of datapoints sent: %s", len(data_buffer))
+        s.close()
+    except socket.error as e:
+        logger.error("Socket error: %s", e)
+
+
+def setup_socket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host, port = os.getenv('DB_HOST'), os.getenv('DB_PORT')
+    logger.debug("Establishing new socket connection to %s:%s", host, port)
+    try:
+        s.connect((str(host), int(port)))
+        logger.debug("Socket connection is fine!")
+    except socket.error as e:
+        logger.error("Socket connection error: %s", e)
+    return s
 
 
 def main():
     consumer = kafka_consumer_blocking(brokers=brokers)
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-
-    # p = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
-    # write_api.write(bucket=bucket, record=p)
-    # data_buffer = []
+    data_buffer = []
 
     for msg in consumer:
         data = PingEvent.from_dict(msg.value)
-        point = create_point(data)
-        write_api.write(bucket=bucket, record=[point])
-        logger.debug(f'Wrote a point: {point}')
-    #     data_buffer.append(data.to_dict())
-    #     if len(data_buffer) % 10 == 0:
-    #         try:
-    #             # s.write_points(data_buffer)
-    #             logger.info("Written %2d points to influx.", len(data_buffer))
-    #             data_buffer = []
-    #         except Exception as e:
-    #             logger.warn("Could not write %s datapoints to influx.", len(data_buffer))
+        data_buffer.append(data)
+        if len(data_buffer) % 10 == 0:
+            try:
+                logger.debug("len(data_buffer): %s", len(data_buffer))
+                handle_multiple_write(data_buffer)
+                data_buffer = []
+            except Exception as e:
+                logger.warning("Could not write %s datapoints.", len(data_buffer))
+                logger.error("Write error: %s", e)
+
+    if len(data_buffer):
+        handle_multiple_write(data_buffer)
+        data_buffer = []
 
 
 if __name__ == '__main__':
     main()
-
